dc5e8a703e358d28926a3c074dfcf5f107df153e
MCUmbrella <mcumbrella@protonmail.com>
2021-10-25 23:45:37 +0800

    javadoc

--- src/main/java/vip/floatationdevice/guilded4j/exception/GuildedException.java
+++ src/main/java/vip/floatationdevice/guilded4j/exception/GuildedException.java
@@ -2,14 +2,23 @@ package vip.floatationdevice.guilded4j.exception;
 
 public class GuildedException extends RuntimeException
 {
-    public String code;
+    private final String code;
 
+    /**
+     * Default constructor.
+     * @param code The error code ("code" key in the JSON).
+     * @param message The error description ("message" key in the JSON).
+     */
     public GuildedException(String code, String message)
     {
         super(message);
         this.code = code;
     }
 
+    /**
+     * Get the error code.
+     * @return The error code in the JSON returned by Guilded API.
+     */
     public String getCode() {return code;}
 
     @Override

ba114193c9b6e7d9d1c5f7f35ba8d640200a7582
MCUmbrella <mcumbrella@protonmail.com>
2022-06-24 09:46:00 +0800

    implement Emote model

---
+++ src/main/java/vip/floatationdevice/guilded4j/object/Emote.java
@@ -0,0 +1,70 @@
+/*
+ Copyright (C) 2021-2022 MCUmbrella & contributors
+ Licensed under the MIT License. See LICENSE in the project root for license information.
+*/
+
+package vip.floatationdevice.guilded4j.object;
+
+import cn.hutool.json.JSONObject;
+import vip.floatationdevice.guilded4j.Util;
+
+/**
+ * The emote object.<br>
+ * <a href="https://www.guilded.gg/docs/api/emote/Emote" target=_blank>https://www.guilded.gg/docs/api/emote/Emote</a>
+ */
+public class Emote
+{
+    private int id;
+    private String name, url;
+
+    /**
+     * Get the ID of the emote.
+     */
+    public int getId(){return id;}
+
+    /**
+     * Get the name of the emote.
+     */
+    public String getName(){return name;}
+
+    public Emote setId(int id)
+    {
+        this.id = id;
+        return this;
+    }
+
+    public Emote setName(String name)
+    {
+        this.name = name;
+        return this;
+    }
+
+    public Emote setUrl(String url)
+    {
+        this.url = url;
+        return this;
+    }
+
+    public static Emote fromJSON(JSONObject json)
+    {
+        Util.checkNullArgument(
+                json.getInt("id"),
+                json.getStr("name"),
+                json.getStr("url")
+        );
+        return new Emote()
+                .setId(json.getInt("id"))
+                .setName(json.getStr("name"))
+                .setUrl(json.getStr("url"));
+    }
+
+    @Override
+    public String toString()
+    {
+        return new JSONObject()
+                .set("id", id)
+                .set("name", name)
+                .set("url", url)
+                .toString();
+    }
+}
--- src/test/java/vip/floatationdevice/guilded4j/G4JTest.java
+++ src/test/java/vip/floatationdevice/guilded4j/G4JTest.java
@@ -1,9 +1,7 @@
 package vip.floatationdevice.guilded4j;
 
 import cn.hutool.json.JSONObject;
-import vip.floatationdevice.guilded4j.object.CalendarEvent;
-
-import java.util.Arrays;
+import vip.floatationdevice.guilded4j.object.Emote;
 
 /**
  * Some temporary test code will go here.
@@ -14,23 +12,11 @@ public class G4JTest
     {
         G4JDebugger.G4JSession s = new G4JDebugger.G4JSession();
         s.restore();
-        CalendarEvent ce = CalendarEvent.fromJSON(new JSONObject("{\n" +
-                "    \"id\": 1,\n" +
-                "    \"serverId\": \"wlVr3Ggl\",\n" +
-                "    \"channelId\": \"00000000-0000-0000-0000-000000000000\",\n" +
-                "    \"name\": \"Surprise LAN party for my wife \uD83E\uDD2B\",\n" +
-                "    \"description\": \"**Don't say anything to her!** She's gonna love playing Call of Duty all night\",\n" +
-                "    \"location\": \"My house!\",\n" +
-                "    \"url\": \"https://www.surprisepartygame.com/\",\n" +
-                "    \"color\": 16106496,\n" +
-                "    \"startsAt\": \"2022-06-16T00:00:00.000Z\",\n" +
-                "    \"duration\": 60,\n" +
-                "    \"createdAt\": \"2021-06-15T20:15:00.706Z\",\n" +
-                "    \"createdBy\": \"Ann6LewA\",\n" +
-                "    \"cancellation\":{\"description\":\"awa\",\"createdBy\":\"1111111\"},\n" +
-                "    \"mentions\":[\"awawawa\",\"aaaa\",\"w\"]\n" +
+        Emote emo = Emote.fromJSON(new JSONObject("{\n" +
+                "  \"id\": 90002551,\n" +
+                "  \"name\": \"doge_gil\",\n" +
+                "  \"url\": \"https://img.guildedcdn.com/asset/Emojis/Custom/doge_gil.webp\"\n" +
                 "}"));
-        System.out.println(new JSONObject(ce.toString()).toStringPretty());
-        System.out.println(Arrays.toString(ce.getMentions()));
+        System.out.println(new JSONObject(emo.toString()).toStringPretty());
     }
 }

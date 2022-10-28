381327ce2910d12b791881d815067eeca5f3a480
MCUmbrella <mcumbrella@protonmail.com>
2022-10-28 20:46:46 +0800

    implement ForumTopicComment

--- src/main/java/vip/floatationdevice/guilded4j/object/ForumTopicComment.java
+++ src/main/java/vip/floatationdevice/guilded4j/object/ForumTopicComment.java
@@ -5,7 +5,113 @@
 
 package vip.floatationdevice.guilded4j.object;
 
+import cn.hutool.json.JSONConfig;
+import cn.hutool.json.JSONObject;
+import vip.floatationdevice.guilded4j.Util;
+
+/**
+ * The comment of a forum topic.<br>
+ * <a href="https://www.guilded.gg/docs/api/forumComments/ForumTopicComment" target=_blank>https://www.guilded.gg/docs/api/forumComments/ForumTopicComment</a>
+ */
 public class ForumTopicComment
 {
-    //TODO
+    int id, forumTopicId;
+    String content, createdAt, createdBy, updatedAt;
+
+    public static ForumTopicComment fromJSON(JSONObject json)
+    {
+        Util.checkNullArgument(
+                json.getInt("id"),
+                json.getStr("content"),
+                json.getStr("createdAt"),
+                json.getStr("createdBy"),
+                json.getInt("forumTopicId")
+        );
+        return new ForumTopicComment()
+                .setId(json.getInt("id"))
+                .setContent(json.getStr("content"))
+                .setCreatedAt(json.getStr("createdAt"))
+                .setCreatedBy(json.getStr("createdBy"))
+                .setUpdatedAt(json.getStr("updatedAt"))
+                .setForumTopicId(json.getInt("forumTopicId"));
+    }
+
+    /**
+     * Get the ID of the forum topic comment.
+     */
+    public int getId(){return id;}
+
+    public ForumTopicComment setId(int id)
+    {
+        this.id = id;
+        return this;
+    }
+
+    /**
+     * Get the ID of the forum topic that the comment belongs to.
+     */
+    public int getForumTopicId(){return forumTopicId;}
+
+    public ForumTopicComment setForumTopicId(int forumTopicId)
+    {
+        this.forumTopicId = forumTopicId;
+        return this;
+    }
+
+    /**
+     * Get the content of the forum topic comment.
+     */
+    public String getContent(){return content;}
+
+    public ForumTopicComment setContent(String content)
+    {
+        this.content = content;
+        return this;
+    }
+
+    /**
+     * Get the ISO 8601 timestamp that the forum topic comment was created at.
+     */
+    public String getCreatedAt(){return createdAt;}
+
+    public ForumTopicComment setCreatedAt(String createdAt)
+    {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    /**
+     * Get the ID of the user who created this forum topic comment.
+     */
+    public String getCreatedBy(){return createdBy;}
+
+    public ForumTopicComment setCreatedBy(String createdBy)
+    {
+        this.createdBy = createdBy;
+        return this;
+    }
+
+    /**
+     * Get the ISO 8601 timestamp that the forum topic comment was created at.
+     */
+    public String getUpdatedAt(){return updatedAt;}
+
+    public ForumTopicComment setUpdatedAt(String updatedAt)
+    {
+        this.updatedAt = updatedAt;
+        return this;
+    }
+
+    @Override
+    public String toString()
+    {
+        return new JSONObject(new JSONConfig().setIgnoreNullValue(true))
+                .set("id", id)
+                .set("content", content)
+                .set("createdAt", createdAt)
+                .set("createdBy", createdBy)
+                .set("forumTopicId", forumTopicId)
+                .set("updatedAt", updatedAt)
+                .toString();
+    }
 }
--- src/test/java/vip/floatationdevice/guilded4j/G4JTest.java
+++ src/test/java/vip/floatationdevice/guilded4j/G4JTest.java
@@ -1,5 +1,6 @@
 package vip.floatationdevice.guilded4j;
 
+import cn.hutool.json.JSONObject;
 import com.google.common.eventbus.Subscribe;
 import vip.floatationdevice.guilded4j.event.*;
 import vip.floatationdevice.guilded4j.misc.*;
@@ -22,15 +23,21 @@ public class G4JTest
     public static void main(String[] args) throws Exception
     {
         //==============================================================
-        s = new G4JDebugger.G4JSession();
+        /*s = new G4JDebugger.G4JSession();
         s.restore();
         c = new G4JClient(s.savedToken).setVerbose(true).setAutoReconnect(true);
         //c.setProxy(new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 59909)));
         c.registerEventListener(new G4JTest()).connectWebSocket(true, null);
-        c.getWebSocketClient().setHeartbeatInterval(10000);
-        Thread.sleep(2000);
-        c.getReactionManager().createContentReaction("6284cada-9d78-4941-803a-3bc38e3de9aa", "1060214396", 90000094);
+        c.getWebSocketClient().setHeartbeatInterval(10000);*/
         //==============================================================
+        System.out.println(new JSONObject(ForumTopicComment.fromJSON(new JSONObject("{\n" +
+                "  \"id\": 1234567890,\n" +
+                "  \"content\": \"Great idea!\",\n" +
+                "  \"createdAt\": \"2021-06-15T20:15:00.706Z\",\n" +
+                "  \"forumTopicId\": 9876543210,\n" +
+                "  \"createdBy\": \"Ann6LewA\",\n" +
+                "  \"updatedAt\": \"2021-06-15T20:15:00.706Z\"" +
+                "}")).toString()).toStringPretty());
     }
 
     @Subscribe
